4cbd03fc216b86363914698c9f24440236f3f517
Neel Agarwal <neelkagarwal@gmail.com>
2025-01-02 12:41:42 -0600

    Changes:
    -Refactor GCS_Fx.py: Add city_generator and api_openWeather functions, enhance data retrieval, and improve API handling
    
    -WeatherPy.ipynb: Added more error handling for varying functionalities in

--- WeatherPy/GCS_Fx.py
+++ WeatherPy/GCS_Fx.py
@@ -6,9 +6,18 @@
 ### <-- IMPORTED FUNCTIONS
 
 # Function Dependencies --> Imported to Jupyter Notebook (WeatherPy.ipynb) through gcs_fx.py
-from time import asctime
+import requests
+import numpy as np
 import matplotlib.pyplot as plt
+from time import asctime, sleep
 from scipy.stats import linregress, pearsonr
+from citipy import citipy   # Install using `pip install citipy` 
+                            # --> Library finds nearest cities to coordinate pairs
+
+# TODO
+#----------------------------------------------------
+# Saved File Data
+
 
 #----------------------------------------------------
 # Plotters --> Call upon constructor functions
@@ -36,8 +45,9 @@ def r_plot(dict, x, y, text_pos = (0, 0), coord_sys = 'data',
     (slope, intercept, r_value, _, _) = linregress(dict[x], dict[y])
     print(f'The r^2-value is: {r_value**2}')
     regress_vals = [val * slope + intercept for val in dict[x]]
+    # TODO:
     (correlation, _) = pearsonr(dict[x], dict[y])
-    print(f'Correlation Coefficient: {correlation}')
+    print(f'Correlation Coefficient: {correlation}') # TODO /->
     axis.plot(dict[x], regress_vals, color = line_color, 
             alpha = line_alpha, label = f'{y} Linear Regression')
     equation = f'y = {round(slope, decimal)}x + {round(intercept, decimal)}'
@@ -57,7 +67,7 @@ def flush(fileName = None, save = False, parent_dir = 'output_data'):
         plt.savefig(outPath)
         plt.show()
     elif save:
-        fileName = 'flushed_fig'
+        fileName = 'untitled_fig'
         outPath = f'{parent_dir}/{fileName}'
         plt.savefig(outPath)
         plt.show()
@@ -67,6 +77,104 @@ def flush(fileName = None, save = False, parent_dir = 'output_data'):
                     '| Be advised - not saving')
     return print(f'| PyPlot flushed:\n' +
                 f'| File saved successfully as {fileName}.')
+
+def city_generator(curve_size = 1500, lat_range:tuple = (-90, 90), lng_range:tuple = (-180, 180)):
+    '''Method to randomly generator geographical coordinates and find their nearest city'''
+    '''SOURCE CODE FROM edX/2U --> Modified for modulated customizable purposes'''
+    cities = []
+    # Create a set of random lat and lng combinations
+    lats = np.random.uniform(lat_range[0], lat_range[1], size = curve_size)
+    lngs = np.random.uniform(lng_range[0], lng_range[1], size = curve_size)
+    lat_lngs = zip(lats, lngs)
+    for coord in lat_lngs:  # Loop finds nearest city for each coordinate pair
+        city = citipy.nearest_city(coord[0], coord[1]).city_name
+        if city not in cities:  # Append unique city to cities (list)
+            cities.append(city)
+    return cities
+
+def api_openWeather(
+    query_list,
+    apiKey,
+    query_remainder = 1000,
+    base_api = f'https://api.openweathermap.org/data/2.5/weather',
+    units = 'metric',
+    force_run = False
+):
+    query_size = len(query_list)
+    queries_postAPI = query_remainder - query_size
+    if force_run == False:
+        if queries_postAPI < 0:
+            print(
+                'Sorry but this API is limited to 1000 requests per day.\n' +
+                f'You\'ve requested {queries_postAPI * -1} queries too many today.\n\n' +
+                f'Please try again with only {query_remainder} queries in your data set.\n' +
+                'Return Error code: -1')        
+            return -1
+        elif (queries_postAPI < 1000):
+            print(
+                f'You\'ve requested {query_size} queries. You have 1000 per day.\n' +
+                f'If you continue you will have only {queries_postAPI} afterwards.')
+            question = 'Are you sure you want to continue? Enter y to continue: '
+            flag = input(question).lower()        
+        flags = {'y': True, 'yes': True, 'continue': True, 'cont': True}
+        if flag in flags:
+            flag = flags[flag]
+    elif force_run == True:
+        flag = True
+    else:
+        print(f'Bad input {force_run}: Quitting early')
+        return -1
+
+    if flag == True:
+        # Setup for API Key, data storage, and logging counters
+        url = f'{base_api}?appid={apiKey}'
+        data = []
+        record_count = 1
+        set_count = 1
+        # Data logging printout
+        print(
+            '\n' +
+            '-----------------------------\n'
+            'Beginning Data Retrieval\n' +
+            '-----------------------------\n')
+        # Loop through all the queries to fetch data (in this case a city's weather data)
+        for i, query in enumerate(query_list):
+            if (i % 50 == 0 and i >= 50):   # Group cities in sets of 50 for logging
+                set_count += 1
+                record_count = 1
+            # Append query info to API-URL!!!
+            query_url = f'{url}&q={query}&units={units}'
+            print(f'Processing Record {record_count} of Set {set_count} | {query}') # Logging printout for each query
+            record_count += 1
+            try:
+                # Run API request for each query - parse out data points and append to output list
+                response = requests.get(query_url).json()
+                data.append({
+                    'City': query,
+                    'Lat': response['coord']['lat'],
+                    'Lng': response['coord']['lon'],
+                    'Max Temp': response['main']['temp_max'],
+                    'Humidity': response['main']['humidity'],
+                    'Cloudiness': response['clouds']['all'],
+                    'Wind Speed': response['wind']['speed'],
+                    'Country': response['sys']['country'],
+                    'Date': response['dt']
+                })
+            # If there's an error, skip the query
+            except:
+                print(f'Query {query} not found. Skipping...')
+                pass
+            sleep(1)    # Pause between queries to avoid rate limiting
+        # Indicate that Data Loading is complete
+        print(
+            '\n' +
+            '-----------------------------\n' +
+            'Data Retrieval Complete\n' +
+            '-----------------------------\n')
+        return data
+    else:
+        print('\nAPI call canceled! \nReturn Code: 0')
+        return 0
 
 
 #----------------------------------------------------
